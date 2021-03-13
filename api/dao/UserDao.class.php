1b00fce6922889be6c60243a999e79c733f67a88
amarell <mujagicamar@gmail.com>
2021-03-13 23:43:08 +0100

    incremental update

--- api/dao/UserDao.class.php
+++ api/dao/UserDao.class.php
@@ -2,7 +2,7 @@
 require_once dirname(__FILE__)."/BaseDao.class.php";
 
 class UserDao extends BaseDao{
-  
+
   public function get_user_by_id($id){
     return $this->query_unique("SELECT * FROM users WHERE user_id=:id", ["id"=>$id]);
   }
@@ -16,11 +16,15 @@ class UserDao extends BaseDao{
   }
 
   public function update_user($id, $user){
-    $sql = "UPDATE users SET name = :name, account_id = :account_id WHERE user_id = :user_id";
-    $stmt= $this->connection->prepare($sql);
-    $user['id'] = $id;
+    $query = "UPDATE users SET ";
+    foreach($user as $name => $value){
+      $query .= $name ."= :".$name.", ";
+    }
+    $query = substr($query, 0, -2);
+    $query .= " WHERE user_id= :user_id";
+    $stmt= $this->connection->prepare($query);
+    $user["user_id"] = $id;
     $stmt->execute($user);
-
     return $user;
   }
 }
--- api/test.php
+++ api/test.php
@@ -9,25 +9,10 @@ require_once dirname(__FILE__)."/dao/AccountDao.class.php";
 $user_dao = new UserDao();
 //$user = $user_dao->get_user_by_email("mujagicamar@gmail.com");
 
-/*
-
 $user = [
-  "name"=>"Ado",
-  "account_id"=>1
+  "name"=>"Amar",
 ];
 
-
-print_r($user_dao->add_user($user));
-
-*/
-
-$account_dao = new AccountDao();
-
-
-
-print_r($account_dao->get_account_by_id(1));
-echo "<br>";
-print_r($account_dao->get_account_by_email("mujagicamar@gmail.com"));
-
+$user_dao->update_user(9,$user);
 
 ?>
