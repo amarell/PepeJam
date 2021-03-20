68092a9f7731f8cabe962fc4329b012051b08791
amarell <mujagicamar@gmail.com>
2021-03-20 13:23:00 +0100

    added parsing of query parameters

--- api/index.php
+++ api/index.php
@@ -6,11 +6,20 @@ error_reporting(E_ALL);
 require_once dirname(__FILE__)."/../vendor/autoload.php";
 require_once dirname(__FILE__)."/dao/BaseDao.class.php";
 require_once dirname(__FILE__)."/dao/UserDao.class.php";
+
+/* Include all routes */
 require_once dirname(__FILE__)."/routes/users.php";
 
-Flight::route('/', function(){
-    echo 'hello world3!';
+/* Utility function for reading parameters from the URL*/
+Flight::map('query', function($name, $default_value = NULL){
+  $request = Flight::request();
+  $query_param = @$request->query->getData()[$name];
+  $query_param = $query_param ? $query_param : $default_value;
+  return $query_param;
 });
+
+/* Register DAO layer */
+Flight::register("user", "UserDao");
 
 Flight::start();
 
--- api/routes/users.php
+++ api/routes/users.php
@@ -1,37 +1,28 @@
 <?php
 
-Flight::register('user', 'UserDao');
-
 Flight::route('GET /user/@id', function($id){
   Flight::json(Flight::user()->get_by_id($id, "user_id"));
 });
 
 Flight::route('PUT /user/@id', function($id){
-
-  $request = Flight::request();
-  $data = $request->data->getData();
-  Flight::user()->update($id, $data, "user_id");
-
+  Flight::user()->update($id, Flight::request()->data->getData(), "user_id");
   $account = Flight::user()->get_by_id($id, "user_id");
-
   Flight::json($account);
 
 });
 
 Flight::route('POST /users', function(){
-  $request = Flight::request();
-
-  $data = $request->data->getData();
-
-
-  $user = Flight::user()->add($data, "user_id");
-
+  $user = Flight::user()->add(Flight::request()->data->getData(), "user_id");
   Flight::json($user);
 });
 
 
 Flight::route('GET /users', function(){
-  Flight::json(Flight::user()->get_all($_GET["offset"] = 0, $_GET["limit"] = 25));
+  $offset = Flight::query("offset", 0);
+  $limit = Flight::query("limit", 10);
+
+  Flight::json(Flight::user()->get_all($offset, $limit));
+  //Flight::json(Flight::user()->get_all($_GET["offset"] = 0, $_GET["limit"] = 25));
 });
 
 ?>
