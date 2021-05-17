be704a9a1dc7265357cd560875214582c6a797e5
amarell <mujagicamar@gmail.com>
2021-05-17 23:51:06 +0200

    added songs table

--- api/index.php
+++ api/index.php
@@ -1,4 +1,9 @@
 <?php
+
+header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Methods: OPTIONS");
+
+
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
--- index.php
+++ index.php
@@ -1,3 +1,6 @@
 <?php
+
+header('Access-Control-Allow-Origin: *');
+
 echo "Hello world!";
 ?>
