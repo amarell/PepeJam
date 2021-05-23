305022e66856f9282ccc34f00e232318e2c30c11
amarell <mujagicamar@gmail.com>
2021-05-23 19:18:44 +0200

    added update song functionality

--- api/dao/BaseDao.class.php
+++ api/dao/BaseDao.class.php
@@ -17,7 +17,7 @@ class BaseDao{
     $this->table = $table;
     $this->primary_key = $primary_key;
     try {
-      $this->connection = new PDO("mysql:host=".Config::DB_HOST().";dbname=".Config::DB_SCHEME(), Config::DB_USERNAME(), Config::DB_PASSWORD());
+      $this->connection = new PDO("mysql:host=".Config::DB_HOST().";port=".Config::DB_PORT().";dbname=".Config::DB_SCHEME(), Config::DB_USERNAME(), Config::DB_PASSWORD());
       $this->connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
     } catch(PDOException $e) {
