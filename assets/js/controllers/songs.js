528c4e250678d4861f93ab5610d50a485d8fca86
amarell <mujagicamar@gmail.com>
2021-06-28 15:25:17 +0200

    testing

--- assets/js/controllers/songs.js
+++ assets/js/controllers/songs.js
@@ -69,7 +69,7 @@ class Song {
             },
             { "data": "song_duration" },
             { "data": "number_of_plays" },
-            { "data": "artist_name" }
+            { "data": "artist_id" }
         ]
       });
   }
